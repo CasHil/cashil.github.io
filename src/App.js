3ca09cfe14a920c3b0d24250500d275fd1a11b1c
Casper Hildebrand <casperhildebrand@gmail.com>
2022-10-03 18:02:43 +0200

    Add framer animations on transition

--- src/App.js
+++ src/App.js
@@ -1,28 +1,25 @@
 import "./App.css";
-import { HashRouter } from "react-router-dom";
+import { BrowserRouter } from "react-router-dom";
 import Header from "./components/Header/Header";
 import Body from "./components/Body/Body";
 import IntroLinks from "./components/Intro/IntroLinks/IntroLinks";
-import { motion } from "framer-motion";
 
 function App() {
   return (
     <div className="bg-gray-900 min-h-screen">
-      <HashRouter>
-        <div class="flex flex-row flex-wrap py-4">
-          <aside class="w-full sm:w-1/4 md:w-1/4 px-2">
-            <div class="sticky top-0 p-4 w-full">
+      <BrowserRouter>
+        <div className="flex flex-row flex-wrap py-4">
+          <aside className="w-full sm:w-1/4 md:w-1/4 px-2">
+            <div className="sticky top-0 p-4 w-full">
               <Header />
               <IntroLinks />
             </div>
           </aside>
-          <div class="w-full sm:w-3/4 md:w-3/4 pt-1 px-2">
-            <motion.div layoutId="underline">
-              <Body />
-            </motion.div>
+          <div className="w-full sm:w-3/4 md:w-3/4 pt-1 px-2">
+            <Body />
           </div>
         </div>
-      </HashRouter>
+      </BrowserRouter>
     </div>
   );
 }
--- src/components/Body/Body.jsx
+++ src/components/Body/Body.jsx
@@ -1,17 +1,57 @@
 import React from "react";
-import { Route, Routes } from "react-router-dom";
+import { Route, Routes, useLocation } from "react-router-dom";
 import Intro from "../Intro/Intro";
 import Education from "../Education/Education";
 import WorkExperience from "../WorkExperience/WorkExperience";
+import { AnimatePresence, motion } from "framer-motion";
 
 export default function Body() {
+  const location = useLocation();
+
+  function wrapMotionDiv(element) {
+    const states = {
+      hidden: {
+        opacity: 0,
+        x: 100,
+      },
+      show: {
+        opacity: 1,
+        x: 0,
+      },
+      exit: {
+        opacity: 0,
+        x: -100,
+      },
+    };
+    return (
+      <motion.div
+        // initial={states.hidden}
+        // animate={states.show}
+        // exit={states.exit}
+        initial={states.hidden}
+        animate={states.show}
+        exit={states.exit}
+        transition={{ duration: 0.3 }}
+      >
+        {element}
+      </motion.div>
+    );
+  }
   return (
     <main className="m-auto pb-5 w-5/6 lg:w-2/3 content flex justify-center flex-col md:block">
-      <Routes>
-        <Route exact path="/" element={<Intro />}></Route>
-        <Route path="/education" element={<Education />}></Route>
-        <Route path="/work-experience" element={<WorkExperience />}></Route>
-      </Routes>
+      <AnimatePresence exitBeforeEnter>
+        <Routes location={location} key={location.pathname}>
+          <Route exact path="/" element={wrapMotionDiv(<Intro />)}></Route>
+          <Route
+            path="/education"
+            element={wrapMotionDiv(<Education />)}
+          ></Route>
+          <Route
+            path="/work-experience"
+            element={wrapMotionDiv(<WorkExperience />)}
+          ></Route>
+        </Routes>
+      </AnimatePresence>
     </main>
   );
 }
